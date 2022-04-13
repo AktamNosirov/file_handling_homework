75e6522a19a1ae1e7875d211b3a737cce3dcf365
Aktam <94_aktam@mail.ru>
2022-04-13 14:31:50 +0300

    2

--- file02.py
+++ file02.py
@@ -6,5 +6,17 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    string=""
+    for i in data :
+        a=str(i)
+        string+=a
+        
+      
+    return len(string) 
 
 # Read data from file
+
+fayl2=open("txt_file/data02.txt","r")
+data=fayl2.read()
+print(main(data))
+fayl2.close()
