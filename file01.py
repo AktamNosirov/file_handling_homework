9f33a61e6d3a36cddc0d69d9dd8cfbec0e747b0d
Aktam <94_aktam@mail.ru>
2022-04-13 14:13:59 +0300

    1

--- file01.py
+++ file01.py
@@ -6,5 +6,17 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    list=[]
+    for i in data.split(",") :
+        a=int(i)
+        list.append(a)
+    
+      
+    return list
 
 # Read data from file
+
+fayl1=open("txt_file\data01.txt","r")
+data=fayl1.read()
+print(main(data))
+fayl1.close()
